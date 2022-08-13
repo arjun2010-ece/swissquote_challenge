8542c4b01015341d4558b05fbb82e793766b672b
Arjun Sah <arjun@appliedblockchain.com>
2022-08-13 21:20:21 +0100

    created the whole apps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState, useEffect, useCallback } from "react";
+import axios from "axios"
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([])
+  const [filteredPosts, setFilteredPosts] = useState([])
+  const [searchText, setSearchText] = useState("")
+  
+  useEffect(() => {
+    const getPosts = async () => {
+      const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      setPosts(data)
+    }
+
+    getPosts();
+
+  }, [])
+
+  const handleChange = useCallback((e) => {
+    let text = e.target.value;
+    const filteredPosts = posts.filter(el => {
+      if(el?.title?.includes(text)){
+        return !!el;
+      }
+      return false;
+    })
+    if(text !== ""){
+      setFilteredPosts(filteredPosts)
+    }
+    setSearchText(e.target.value)
+  }, [posts])
+
+  const handleClick = useCallback(() => {
+    setSearchText("")
+  }, [])
+
+  const postList = posts && posts.map(el => (
+    <div key={el.title} className="posts">
+      <p>{el.title}</p>
+    </div>
+  ))
+
+  const filteredPostList = filteredPosts && filteredPosts.map(el => (
+    <div key={el.title} className="posts">
+      <p>{el.title}</p>
+    </div>
+  ))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" name="search" value={searchText}
+        onChange={handleChange}
+        />
+        <button onClick={handleClick}>Clear</button>
+      </div>
+      {searchText === "" ? postList : filteredPostList}
     </div>
   );
 }
